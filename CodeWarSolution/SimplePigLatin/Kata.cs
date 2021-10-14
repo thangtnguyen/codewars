c358c202a3b1ae2479b1d814dfdc2363cd941f23
Thang The Nguyen <thangnguyen0722@gmail.com>
2021-10-14 15:08:49 +0700

    Updated code for SimplePigLatin

---
+++ CodeWarSolution/SimplePigLatin/Kata.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePigLatin
+{
+    public class Kata
+    {
+        public static string PigIt(string str)
+        {
+            StringBuilder result = new StringBuilder();
+            const string suffix = "ay";
+            const string space = " ";
+
+            foreach (var item in str.Split(space))
+            {
+                if (item.Trim().Length == 1)
+                {
+                    if (Char.IsLetter(item[0]))
+                    {
+                        result.Append(item).Append(suffix).Append(space);
+                    }
+                    else
+                    {
+                        result.Append(item).Append(space);
+                    }
+                }
+                else
+                {
+                    var firstString = item.Substring(0, 1);
+                    var remainingString = item.Substring(1);
+                    if (char.IsLetter(item[0]))
+                    {
+                        result.Append(remainingString).Append(firstString).Append(suffix).Append(space);
+                    }
+                    else
+                    {
+                        result.Append(item).Append(space);
+                    }
+                }
+            }
+            
+            return result.ToString().Trim();
+        }
+    }
+}
